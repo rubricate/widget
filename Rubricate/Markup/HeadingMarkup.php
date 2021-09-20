aa1db448e2b8899763115fb37e17ef022072fad9
Estefanio N Santos <estefanions@gmail.com>
2021-09-20 20:58:37 -0300

    renamed: HeadingStr -> Heading

---
+++ Rubricate/Markup/HeadingMarkup.php
@@ -0,0 +1,28 @@
+<?php 
+
+/*
+ * @package     RubricatePHP
+ * @author      Estefanio N Santos <estefanions AT gmail DOT com>
+ * @link        https://github.com/rubricate/markup
+ * 
+ */
+
+namespace Rubricate\Markup;
+
+
+class HeadingMarkup extends AbstractStrMarkup
+{
+
+    public function __construct($text, $level = 1)
+    {
+        self::setLevel($level);
+        parent::__construct($text);
+    }
+
+
+    private function setLevel($level)
+    {
+        $this->tagName = 'h' . $level;    
+    } 
+}
+
