adc03270f8067356679f0f101dbb1ae58fb7fa09
Estefanio N Santos <estefanions@gmail.com>
2021-09-20 21:01:21 -0300

    renamed: ParagraphStr -> Paragraph

---
+++ Rubricate/Markup/ParagraphMarkup.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+ * @package     RubricatePHP
+ * @author      Estefanio N Santos <estefanions AT gmail DOT com>
+ * @link        https://github.com/rubricate/markup
+ * 
+ */
+
+namespace Rubricate\Markup;
+
+
+class ParagraphMarkup extends AbstractStrMarkup
+{
+    protected $tagName = 'p';
+}
+
