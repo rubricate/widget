6043b0f36be63767e6800c5151b4b943bd3d6316
Estefanio N Santos <estefanions@gmail.com>
2022-10-23 22:11:33 -0300

    RENAMED - HeadingStrWidget -> HeadingWidget

---
+++ Rubricate/Widget/HeadingWidget.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace Rubricate\Widget;
+
+class HeadingWidget extends AbstractStrWidget
+{
+    public function __construct($text, $level = 1)
+    {
+        self::setLevel($level);
+        parent::__construct($text);
+    }
+
+    private function setLevel($level): void
+    {
+        if(!in_array($level, [1, 2, 3, 4, 5, 6])){
+
+            throw new \Exception(PHP_EOL 
+                . '"Second Invalid Argument: '
+                . 'use 1, 2, 3, 4, 5 or 6"'
+                . PHP_EOL
+            );
+        }
+
+        $this->tagName = 'h' . $level;    
+    } 
+}
+
