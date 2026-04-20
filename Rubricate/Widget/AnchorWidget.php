9ad122264a88bbcb80a3d111234bc9fe0597daf6
Estefanio N Santos <estefanions@gmail.com>
2026-04-20 16:20:31 -0300

    [#2604201528] refactor: upgrade AnchorWidget to PHP 8.2
    - Added readonly modifier to the internal CreateElement instance.
    - Implemented strict typing for href and attribute methods.
    - Updated return types to self for better method chaining.
    - Improved text handling using mixed type and null check.

--- Rubricate/Widget/AnchorWidget.php
+++ Rubricate/Widget/AnchorWidget.php
@@ -4,38 +4,40 @@ declare(strict_types=1);
 
 namespace Rubricate\Widget;
 
+use Exception;
 use Rubricate\Element\CreateElement;
 use Rubricate\Element\IGetElement;
 use Rubricate\Element\StrElement;
 
 class AnchorWidget implements IGetElement
 {
-    private $e;
+    private readonly CreateElement $e;
 
-    public function __construct($href, $text = '')
+    public function __construct(string $href, mixed $text = null)
     {
         $this->e = new CreateElement('a');
         $this->e->setAttribute('href', $href);
-        $this->e->addChild(new StrElement($text));
+        
+        if ($text !== null) {
+            $this->e->addChild(new StrElement($text));
+        }
     }
 
-    public function setAttribute($key, $value = null): object
+    public function setAttribute(string $name, mixed $value = null): self
     { 
-        if($key == 'href') {
-            throw new \Exception(''
-                ."The 'href' attribute was used on the instance"
-                . PHP_EOL 
+        if ($name === 'href') {
+            throw new Exception(
+                "The 'href' attribute is already defined in the constructor."
             );
         }
 
-        $this->e->setAttribute($key, $value);
+        $this->e->setAttribute($name, $value);
         return $this;
     } 
 
-    public function addChild(IGetElement $e): object
+    public function addChild(IGetElement $e): self
     {
         $this->e->addChild($e);
-
         return $this;
     } 
 
@@ -43,6 +45,5 @@ class AnchorWidget implements IGetElement
     {
         return $this->e->getElement();
     } 
-    
 }
 
